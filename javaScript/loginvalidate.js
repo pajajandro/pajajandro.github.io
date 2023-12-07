02fe3d3ec1a76c3c2cc3cbcbe5f8d1e22d1ee759
pajajandro <93785591+pajajandro@users.noreply.github.com>
2023-12-07 19:51:23 +0100

    Add files via upload

---
+++ javaScript/loginvalidate.js
@@ -0,0 +1,16 @@
+function validateForm() {
+    var password = document.getElementById('confirmPassword').value;
+    var username = document.getElementById('username').value;
+
+    if (password.length < 7) {
+        alert('La contraseña debe tener al menos 8 caracteres.');
+        return false;
+    }
+
+    if (password !== "3022nit" && username !== "nitMirabilis") {
+        alert('Las contraseñas o el usuario no coinciden.');
+        return false;
+    }
+
+    return true;
+}
